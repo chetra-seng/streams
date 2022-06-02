00d9bcad0e39d8258da4c1e005d0f3b4f7602fec
chetra_seng <sengchetra.love@icloud.com>
2022-06-02 21:09:25 +0700

    Added sample routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamList from './StreamList';
+import StreamCreate from './StreamCreate';
+import StreamEdit from './StreamEdit';
+import StreamDelete from './StreamDelete';
+import StreamShow from './StreamShow';
 
 const App = () => {
-    return <div>App</div>
+  return(
+		<div>
+			<BrowserRouter>
+				<Route path='/' exact component={StreamList} />
+				<Route path='/streams/new' exact component={StreamCreate} />
+				<Route path='/streams/edit' exact component={StreamEdit} />
+				<Route path='/streams/delete' exact component={StreamDelete} />
+				<Route path='/streams/show' exact component={StreamShow} />
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App;
