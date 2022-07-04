0fe74db5ba251f3956630cded114bf043f14ed41
chetra_seng <sengchetra.love@icloud.com>
2022-07-04 20:47:36 +0700

    Added Simple GoogleAuth

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import StreamList from './StreamList';
 import StreamCreate from './StreamCreate';
 import StreamEdit from './StreamEdit';
 import StreamDelete from './StreamDelete';
 import StreamShow from './StreamShow';
+import Header from "./Header";
 
 const App = () => {
   return(
 		<div>
 			<BrowserRouter>
+				<Header />
 				<Route path='/' exact component={StreamList} />
 				<Route path='/streams/new' exact component={StreamCreate} />
 				<Route path='/streams/edit' exact component={StreamEdit} />
---
+++ client/src/components/GoogleAuth.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+const GoogleAuth = () => {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+  useEffect(() => {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "493172449628-188kr9vobpnohm65sh5373la6m6cokrv.apps.googleusercontent.com",
+          scope: "email",
+          plugin_name: "streams",
+        })
+        .then(() => {
+          const auth = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(auth.isSignedIn.get());
+          auth.isSignedIn.listen(signedIn => {
+            setIsSignedIn(signedIn);
+          })
+        });
+    });
+  }, []);
+
+  const isSignedHandler = () => {
+    if(isSignedIn === null){
+      return 'Unknown'
+    }
+    else if(isSignedIn){
+      return 'Signed in'
+    }
+    else{
+      return 'Not signed in'
+    }
+  }
+
+  return <div>{isSignedHandler()}</div>;
+};
+
+export default GoogleAuth;
